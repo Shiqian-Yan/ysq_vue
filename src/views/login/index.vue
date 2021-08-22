09572c21bd9fdf0d2af732bce9a39adfc9c276ae
ysq <1078735768@qq.com>
2021-08-22 19:39:56 +0800

    ysqvue1

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-container">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-      <h3 class="title">谷粒学院后台管理系统</h3>
+      <h3 class="title">ysq博客后台管理系统</h3>
       <el-form-item prop="username">
         <span class="svg-container">
           <svg-icon icon-class="user" />
@@ -28,10 +28,6 @@
           登录
         </el-button>
       </el-form-item>
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: 111111</span>
-      </div>
     </el-form>
   </div>
 </template>
